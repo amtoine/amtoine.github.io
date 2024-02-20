970a865ab8bc62a5aa04b188be34c5092512bfd6
amtoine <stevan.antoine@gmail.com>
2024-02-20 19:40:21 +0100

    add colors to the normalized plot

--- content/posts/my-mario-kart-8-deluxe-adventure/plot-normalized.py
+++ content/posts/my-mario-kart-8-deluxe-adventure/plot-normalized.py
@@ -4,7 +4,24 @@ import os
 
 values = list(map(float, sys.argv[1:]))
 
-plt.plot(values)
+colors = {
+    "axes.edgecolor": "orange",
+    "xtick.color": "red",
+    "ytick.color": "green",
+    "figure.facecolor": "white",
+}
+
+with plt.rc_context(colors):
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    fig.canvas.manager.full_screen_toggle()
+    fig.patch.set_facecolor((31 / 255, 34 / 255, 42 / 255))
+    ax.xaxis.label.set_color(colors["xtick.color"])
+    ax.yaxis.label.set_color(colors["ytick.color"])
+    ax.plot(values)
+
+plt.grid(True)
+
 plt.savefig(
     os.path.join(os.path.dirname(__file__), "normalized-scores.png"),
+    dpi=500,
 )
