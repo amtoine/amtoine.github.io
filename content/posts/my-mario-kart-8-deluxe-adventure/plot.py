dd7791e2c0b0aa1703535d6a8939ab3da925d16e
amtoine <stevan.antoine@gmail.com>
2024-02-20 12:56:33 +0100

    better and more general label placement

--- content/posts/my-mario-kart-8-deluxe-adventure/plot.py
+++ content/posts/my-mario-kart-8-deluxe-adventure/plot.py
@@ -6,6 +6,8 @@ import json
 data = list(map(int, sys.argv[1].split("\n")))
 days = json.loads(sys.argv[2])
 
+MIN_SCORE = 1_000
+
 colors = {
     "axes.edgecolor": "orange",
     "xtick.color": "red",
@@ -22,12 +24,13 @@ with plt.rc_context(colors):
     indices = [day["index"] for day in days]
     scores = [day["score"] for day in days]
     last_index = max(indices)
+    full_score_range = max(scores) - MIN_SCORE
     ax.scatter(indices, scores)
     for (t, x, y) in zip(dates, indices, scores):
-        if x < last_index * 0.9:
-            offset = (x + 10, y - 100)
+        if x < last_index * 0.85:
+            offset = (x + last_index * 0.1, y - full_score_range * 0.05)
         else:
-            offset = (x - 50, y - 100)
+            offset = (x - last_index * 0.4, y - full_score_range * 0.05)
         ax.annotate(
             t,
             (x, y),
@@ -40,7 +43,7 @@ with plt.rc_context(colors):
 plt.grid(True)
 
 plt.xlim([0, None])
-plt.ylim([1_000, None])
+plt.ylim([MIN_SCORE, None])
 
 plt.savefig(
     os.path.join(os.path.dirname(__file__), "scores.png"),
